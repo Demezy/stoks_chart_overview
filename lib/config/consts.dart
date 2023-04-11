73432e27aa20281d2d25ecc0890b3d43570d7b98
Demezy <38487319+Demezy@users.noreply.github.com>
2023-04-11 19:49:27 +0300

    feat: app global constans

---
+++ lib/config/consts.dart
@@ -0,0 +1,14 @@
+class Consts {
+  static const List<String> trackedCompanySymbols = [
+    'AAPL',
+    'AMZN',
+    'GOOG',
+    'MSFT',
+    'FB',
+  ];
+  
+  static int apiRequestDelayMillisec = 1000;
+  static String apiKey = 'demo';
+
+  Consts._();
+}
