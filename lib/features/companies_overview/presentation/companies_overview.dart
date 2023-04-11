43dca122eb11b692f4a6aaf7b38d5d29279d3d0e
Demezy <38487319+Demezy@users.noreply.github.com>
2023-04-11 21:35:19 +0300

    feat: main screen ui improvement and navigation

--- lib/features/companies_overview/presentation/companies_overview.dart
+++ lib/features/companies_overview/presentation/companies_overview.dart
@@ -1,8 +1,11 @@
+import 'dart:math';
+
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:stoks_chart_overview/features/companies_overview/data/companies_overview_providers.dart';
+import 'package:stoks_chart_overview/config/consts.dart';
 import 'package:stoks_chart_overview/features/companies_overview/data/company_detales_updater.dart';
 
 class CompaniesOverview extends HookConsumerWidget {
@@ -10,54 +13,59 @@ class CompaniesOverview extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final touchedIndex = useState<int>(0);
-    final companyDetales = ref.watch(companyDetalesUpdaterProvider);
-    return AspectRatio(
-      aspectRatio: 1.3,
+    final hoveredIndex = useState<int>(0);
+    final companyesDetails = ref.watch(companyDetalesUpdaterProvider);
+    if (companyesDetails.isEmpty) {
+      return const Center(child: CircularProgressIndicator());
+    }
+    return Material(
       child: AspectRatio(
-        aspectRatio: 1,
-        child: PieChart(
-          PieChartData(
-            pieTouchData: PieTouchData(
-              touchCallback: (FlTouchEvent event, pieTouchResponse) {
-                if (!event.isInterestedForInteractions ||
-                    pieTouchResponse == null ||
-                    pieTouchResponse.touchedSection == null) {
-                  touchedIndex.value = -1;
-                  return;
-                }
-                touchedIndex.value =
-                    pieTouchResponse.touchedSection!.touchedSectionIndex;
-              },
+        aspectRatio: 1.3,
+        child: AspectRatio(
+          aspectRatio: 1,
+          child: PieChart(
+            PieChartData(
+              pieTouchData: PieTouchData(
+                touchCallback: (event, pieTouchResponse) {
+                  if (!event.isInterestedForInteractions ||
+                      pieTouchResponse == null ||
+                      pieTouchResponse.touchedSection == null) {
+                    hoveredIndex.value = -1;
+                    return;
+                  }
+                  final sectionIndex =
+                      pieTouchResponse.touchedSection!.touchedSectionIndex;
+                  hoveredIndex.value = sectionIndex;
+                  if (event is FlTapUpEvent) {
+                    context.push(
+                      Routes.detales,
+                      extra: companyesDetails[sectionIndex],
+                    );
+                  }
+                },
+              ),
+              borderData: FlBorderData(
+                show: false,
+              ),
+              sectionsSpace: 0,
+              centerSpaceRadius: 0,
+              sections: List<PieChartSectionData>.generate(
+                companyesDetails.length,
+                (index) {
+                  final companyDetail = companyesDetails[index];
+                  final isHovered = index == hoveredIndex.value;
+                  return PieChartSectionData(
+                    value: companyDetail.marketCapitalisation.toDouble(),
+                    radius: isHovered ? 120 : 100,
+                    color: Color(Random(companyDetail.symbol.hashCode)
+                        .nextInt(0xffffffff)),
+                    title: companyDetail.symbol,
+                  );
+                },
+              ),
             ),
-            borderData: FlBorderData(
-              show: false,
-            ),
-            sectionsSpace: 0,
-            centerSpaceRadius: 0,
-            sections: companyDetales
-                .map(
-                  (companyDetail) => PieChartSectionData(
-                    value:companyDetail.marketCapitalisation as double,
-                  ),
-                )
-                .toList(),
           ),
         ),
-      ),
-    );
-  }
-
-  @override
-  Widget build2(BuildContext context, WidgetRef ref) {
-    final companyRepository = ref.watch(companyRepositoryProvider);
-    companyRepository
-        .getTrackedCompanies()
-        .then((companies) => companies.forEach(print));
-
-    return Scaffold(
-      body: Container(
-        color: Colors.grey,
       ),
     );
   }
