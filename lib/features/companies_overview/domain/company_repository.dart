35fe851e639f2b10c72576ae1ba5e705d1dac5b7
Demezy <38487319+Demezy@users.noreply.github.com>
2023-04-12 00:30:05 +0300

    feat: add getMaybeTrackedCompanies in CompanyRepository

--- lib/features/companies_overview/domain/company_repository.dart
+++ lib/features/companies_overview/domain/company_repository.dart
@@ -1,6 +1,9 @@
-import 'package:stoks_chart_overview/features/companies_overview/domain/company_detales.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:stoks_chart_overview/features/companies_overview/domain/company_details.dart';
 
 abstract class CompanyRepository {
-  Future<CompanyDetales> getCompanyDetalesBySym(String companySym);
-  Future<List<CompanyDetales>> getTrackedCompanies();
+  Future<CompanyDetails> getCompanyDetailsBySym(String companySym);
+  Future<List<CompanyDetails>> getTrackedCompanies();
+  // Return either success info or String of usuccessful sym. Cathes api exceptions.
+  Future<List<AsyncValue<CompanyDetails>>> getMaybeTrackedCompanies();
 }
