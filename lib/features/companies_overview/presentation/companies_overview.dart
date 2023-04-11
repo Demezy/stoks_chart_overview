885e7062a20b782173ef083567e2fe63f4ade2d7
Demezy <38487319+Demezy@users.noreply.github.com>
2023-04-12 00:36:23 +0300

    feat: use safe version of api notifier in ui

--- lib/features/companies_overview/presentation/companies_overview.dart
+++ lib/features/companies_overview/presentation/companies_overview.dart
@@ -8,7 +8,7 @@ import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:stoks_chart_overview/config/consts.dart';
 import 'package:stoks_chart_overview/exceptions/api_exception.dart';
-import 'package:stoks_chart_overview/features/companies_overview/data/company_details_updater.dart';
+import 'package:stoks_chart_overview/features/companies_overview/data/company_detales_updater_safe.dart';
 
 class CompaniesOverview extends HookConsumerWidget {
   const CompaniesOverview({super.key});
@@ -16,7 +16,8 @@ class CompaniesOverview extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final hoveredIndex = useState<int>(0);
-    final companyesDetails = ref.watch(companyDetailsUpdaterProvider);
+    final companyesDetails = ref.watch(companyDetailsUpdaterProviderSafe);
+    final pieSize = min(MediaQuery.of(context).size.width / 2, 300).toDouble();
     return Scaffold(
       appBar: AppBar(
         title: const Text('main page'),
@@ -32,55 +33,89 @@ class CompaniesOverview extends HookConsumerWidget {
           }
           return Text('$error');
         },
-        data: (details) {
-          return AspectRatio(
-            aspectRatio: 1.3,
-            child: AspectRatio(
-              aspectRatio: 1,
-              child: PieChart(
-                PieChartData(
-                  pieTouchData: PieTouchData(
-                    touchCallback: (event, pieTouchResponse) {
-                      if (!event.isInterestedForInteractions ||
-                          pieTouchResponse == null ||
-                          pieTouchResponse.touchedSection == null) {
-                        hoveredIndex.value = -1;
-                        return;
-                      }
-                      final sectionIndex =
-                          pieTouchResponse.touchedSection!.touchedSectionIndex;
-                      hoveredIndex.value = sectionIndex;
-                      if (event is FlTapUpEvent) {
-                        context.push(
-                          '/${Routes.detales}',
-                          extra: details[sectionIndex],
-                        );
-                      }
-                    },
-                  ),
-                  borderData: FlBorderData(
-                    show: false,
-                  ),
-                  sectionsSpace: 0,
-                  centerSpaceRadius: 0,
-                  sections: List<PieChartSectionData>.generate(
-                    details.length,
-                    (index) {
-                      final detail = details[index];
-                      final isHovered = index == hoveredIndex.value;
-                      return PieChartSectionData(
-                        value: detail.marketCapitalisation.toDouble(),
-                        radius: isHovered ? 120 : 100,
-                        color: Color(
-                          Random(detail.symbol.hashCode).nextInt(0xffffffff),
-                        ),
-                        title: detail.symbol,
-                      );
-                    },
+        data: (asyncDetails) {
+          final details = asyncDetails
+              .where((asyncDetail) => asyncDetail.hasValue)
+              .toList();
+          final failedDetails = asyncDetails
+              .where((asyncDetail) => asyncDetail.hasError)
+              .toList();
+          if (asyncDetails.isEmpty) {
+            return Text(const ApiException.apiUnavailable().message());
+          }
+          return ListView(
+            children: [
+              SizedBox(
+                width: pieSize,
+                height: pieSize,
+                // aspectRatio: 1.3,
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: PieChart(
+                    PieChartData(
+                      pieTouchData: PieTouchData(
+                        touchCallback: (event, pieTouchResponse) {
+                          if (!event.isInterestedForInteractions ||
+                              pieTouchResponse == null ||
+                              pieTouchResponse.touchedSection == null) {
+                            hoveredIndex.value = -1;
+                            return;
+                          }
+                          final sectionIndex = pieTouchResponse
+                              .touchedSection!.touchedSectionIndex;
+                          hoveredIndex.value = sectionIndex;
+                          if (event is FlTapUpEvent) {
+                            context.push(
+                              '/${Routes.detales}',
+                              extra: details[sectionIndex].value,
+                            );
+                          }
+                        },
+                      ),
+                      borderData: FlBorderData(
+                        show: false,
+                      ),
+                      sectionsSpace: 0,
+                      centerSpaceRadius: 0,
+                      sections: List<PieChartSectionData>.generate(
+                        details.length,
+                        (index) {
+                          final detail = details[index].value!;
+                          final isHovered = index == hoveredIndex.value;
+                          return PieChartSectionData(
+                            value: detail.marketCapitalisation.toDouble(),
+                            radius: isHovered ? 120 : 100,
+                            color: Color(
+                              Random(detail.symbol.hashCode)
+                                  .nextInt(0xffffffff),
+                            ),
+                            title: detail.symbol,
+                          );
+                        },
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
+              ...List.generate(
+                failedDetails.length,
+                (index) {
+                  final error = failedDetails[index].asError?.error;
+                  return Card(
+                    child: Row(
+                      children: [
+                        const Icon(Icons.error_outline),
+                        Text(
+                          error is ApiException
+                              ? error.message()
+                              : error.toString(),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ],
           );
         },
       ),
